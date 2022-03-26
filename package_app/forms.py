83132b81559748ff133ea1365189db40602b32c4
mszsabab <mszsabab22@gmail.com>
2022-03-26 16:38:13 +0600

    form done

--- package_app/forms.py
+++ package_app/forms.py
@@ -2,7 +2,5 @@ from django import forms
 
 
 class Form(forms.Form):
-    # id = forms.CharField(max_length=10)
     service = forms.MultipleChoiceField(choices=[("domain", "Domain"), ("internet", "Internet"), ("hosting", "Hosting")], widget=forms.CheckboxSelectMultiple())
-    # price =
     addon = forms.MultipleChoiceField(choices=[("online bill pay", "Online Bill Pay"), ("real IP", "Real IP"), ("24/7 support", "24/7 Support")], widget=forms.CheckboxSelectMultiple())
